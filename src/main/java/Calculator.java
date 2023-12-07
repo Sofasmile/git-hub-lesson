9ba2c5a1e9c2f02b5c01a5070b830ed98f63d6ee
sofiadiakonova <sofiya.dyakonova@ukr.net>
2023-12-07 10:25:32 +0200

    added new double multiply method (h-6)

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -11,6 +11,10 @@ public class Calculator {
         return a * b;
     }
 
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
     public int subtract(int a, int b) {
         return a - b;
     }
